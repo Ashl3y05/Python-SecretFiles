be398a5c0b5f6bb67b38b87eb6b2a41b74fb6c56
Ashley <panelo.ashley@gmail.com>
2025-07-24 23:45:51 +0800

    Initial Commit

---
+++ main.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField, SubmitField
+from wtforms.validators import DataRequired
+
+app = Flask(__name__)
+
+app.secret_key = "i_am_secret"
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField("Login")
+
+@app.route("/")
+def home():
+    form = LoginForm()
+    return render_template('index.html', form=form)
+
+@app.route("/login", methods=["GET","POST"])
+def login():
+    login_form = LoginForm()
+    login_form.validate_on_submit()
+    return  render_template('login.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
