96aaea9eef929dfa869debb2e85c830895cd5eb3
Ashley <panelo.ashley@gmail.com>
2025-07-26 00:02:55 +0800

    Fixed the login function to return itself

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, InputRequired
 
 app = Flask(__name__)
 
 app.secret_key = "i_am_secret"
 
 class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    email = StringField('Email', validators=[InputRequired()])
+    password = PasswordField('Password', validators=[InputRequired()])
     submit = SubmitField("Login")
 
 @app.route("/")
@@ -21,7 +21,7 @@ def home():
 def login():
     login_form = LoginForm()
     login_form.validate_on_submit()
-    return render_template('login.html', form=login_form)
+    return render_template('index.html', form=login_form)
 
 if __name__ == '__main__':
     app.run(debug=True)
