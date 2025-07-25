8d925bd7532c342b7099665e5c0bffcc4cffbc1b
Ashley <panelo.ashley@gmail.com>
2025-07-26 00:16:06 +0800

    Forms now check for proper format and length

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField, SubmitField
-from wtforms.validators import DataRequired, InputRequired
+from wtforms.validators import InputRequired, Email, Length
 
 app = Flask(__name__)
 
 app.secret_key = "i_am_secret"
 
 class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[InputRequired()])
-    password = PasswordField('Password', validators=[InputRequired()])
+    email = StringField('Email', validators=[InputRequired(), Email(message="Invalid Email format")])
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=8)])
     submit = SubmitField("Login")
 
 @app.route("/")
